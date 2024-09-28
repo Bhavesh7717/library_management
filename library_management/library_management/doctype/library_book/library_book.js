c68fabde22a7f27eba7fe294581724d3865f0dfc
tusharpatil <tusharpatil3688@gmail.com>
2024-09-28 15:01:41 +0530

    Hemangi: stock balance set in library book

--- library_management/library_management/doctype/library_book/library_book.js
+++ library_management/library_management/doctype/library_book/library_book.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Library Book', {
-	// refresh: function(frm) {
-
-	// }
+	no_of_copies: function(frm) {
+		frm.set_value('stock_balance',frm.doc.no_of_copies);
+	}
 });
