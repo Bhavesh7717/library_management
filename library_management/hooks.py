a59f99b8a03ab42b424cf6fb85787ea673bd28d9
tusharpatil <tusharpatil3688@gmail.com>
2024-09-28 14:58:43 +0530

    Hemangi: Added changed hooks file

--- library_management/hooks.py
+++ library_management/hooks.py
@@ -29,7 +29,11 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+    "Employee" : "public/js/employee.js",
+    "Customer" : ["public/js/customer.js","public/js/hemangi_customer.js","public/js/bhavesh_customer.js", "public/js/tushar_customer.js"]
+    }
+# doctype_js = {"Customer" : "public/js/customer.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
